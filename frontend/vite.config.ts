567f00112ccc054941a113e9d3d6d34395d2814a
Ece Eren <aybikeceren@gmail.com>
2025-02-05 19:34:04 +0300

    Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://api:3000",
+        target: process.env.VITE_API_URL || "http://api:3000",
         changeOrigin: true,
       },
     },
